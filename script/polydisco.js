a17b43df591f95e6b2f3304f52f15751b9746779
ReinerYaeger <chevking101@gmail.com>
2021-11-15 22:05:19 +0000

    Only Needs Task 14 Up

--- script/polydisco.js
+++ script/polydisco.js
@@ -2,9 +2,13 @@ var PlayersData;
 var correctAns;
 var usrCorrectAns = 0;
 var usrIncorrectAns = 0;
+var usrQuestionCount=0;
 var seed1;
 var seed2;
 var QuestionCounter = 0;
+var Questions=[seed1,seed2,correctAns];
+var playerDataBuffer = [""];
+
 
 // Task 2
 function Register(){
@@ -20,28 +24,30 @@ function Register(){
         document.getElementById("notice").innerHTML = "";
     }
     else{
-        notice.innerHTML = "<span style= 'font-weight:bold; ;color: red;'>Please Complete all fields </span>";
+        notice.innerHTML = "<span style= 'font-weight:bold; ;color: red;'>Please Complete all fields </span><hr>";
         document.getElementById("nameForm").scrollIntoView();
         return false;
     }
 
     PlayersData = [fName,lName,age,gender,email,usrCorrectAns,usrIncorrectAns];
     // Apart Task 3
-    disableInput();
+    enableInput();
 }
 
 function disableModal(){ //Disabling modal and activating form and percentage button
-    var endBtn = document.getElementById('modalBg');
-    endBtn.classList.remove('activateModal');
+    document.getElementById('modalBg').classList.remove('activateModal');
+    document.getElementById('modalBgChart').classList.remove('activateModal');
 
+}
+
+
+function resetForm(){
     var input =document.getElementsByClassName('ffrom');
     var startButton = document.getElementById("startBtn");
     var registrationForm = document.forms["registrationForm"];
-    var percentage = document.getElementById("showpercentage");
-    var percentagelb = document.getElementById("showpercentageLabel");
-    var allplayertxt = document.getElementById("showallplayers");
-    var allplayerlb = document.getElementById("showallplayerslb");
-
+    var percentage = document.getElementById("showPercentage");
+    var percentagelb = document.getElementById("showPercentageLb");
+    
     registrationForm.reset();
     input.disabled = false;
 
@@ -51,10 +57,21 @@ function disableModal(){ //Disabling modal and activating form and percentage bu
     startButton.disabled = true;
     percentage.removeAttribute("hidden");
     percentagelb.removeAttribute("hidden");
-    allplayerlb.removeAttribute("hidden");
-    allplayertxt.removeAttribute("hidden");
-    allplayertxt.value += (PlayersData+"\n");
-    document.getElementById("addBreak").innerHTML = "<br>";
+    disableModal();
+}
+
+function showall(){
+    var allplayerlb = document.getElementById("showAllPlayersLb");
+    var hideTextarea = document.getElementById("hideTextarea");
+    hideTextarea.classList.remove('hideTextarea');
+    
+    allplayertxt.innerHTML = ""; 
+    
+    playerDataBuffer.push("\n"+PlayersData);
+    console.log(playerDataBuffer);
+
+    allplayertxt.innerHTML = (playerDataBuffer+"\n");
+    document.getElementsByClassName("addBreak").innerHTML = "<br>";
 }
 
 // Apart of Task 2
@@ -67,49 +84,71 @@ function calculateAge(){
 }
 
 // Apart Task 3
-function disableInput(){
+function enableInput(){
     var input =document.getElementsByClassName('ffrom');
-    var startButton = document.getElementById("startBtn");
-
+    var startButton = document.getElementById('startBtn');
+    var percentScoreBtn = document.getElementById('percentScoreBtn');
     input.disabled = true;
 
     for(i=0;i<input.length;i++){
         input[i].disabled=true;
     }   
     startButton.disabled = false;
+    percentScoreBtn.disabled = false;
+
 }
 
 //Task 4
 function PlayGame() {
-    
-    document.getElementById("answerResponse").innerHtml = "";
+    document.getElementById("answerResponse").innerHtml = " ";
+
     document.getElementById('modalBg').className += ' activateModal';
 
     var seed1 = Math.floor(Math.random()*9)+1;
     var seed2 = Math.floor(Math.random()*5)+1;
     correctAns = seed1 * seed2;
+    Questions.push(seed1);
+    Questions.push(seed2);
+    Questions.push(correctAns);
+    console.log(Questions);
     QuestionCounter++;
         
     document.getElementById("numvalue1").value = seed1;
     document.getElementById("numvalue2").value = seed2;
-    document.getElementById("usrAns").value  = "";
+    document.getElementById("usrAns").value  = " ";
 }
 //TASK 6	
 function checkAnswer(){
     var usrAns = document.getElementById('usrAns').value;
+    document.getElementById("answerResponse").innerHTML = "";
                 //  0      1    2    3     4       5             6
     //PlayersData = [fName,lName,age,gender,email,usrCorrectAns,usrIncorrectAns];
-    console.log("Correct: "+usrCorrectAns);
-    console.log("Incorrect: " + usrIncorrectAns);
-	
     if(usrAns == correctAns){
         document.getElementById("answerResponse").innerHTML = "<span style='color: green'>CORRECT!</span>";
         usrCorrectAns+=1;
         PlayersData.splice((PlayersData.length)-2,1,usrCorrectAns);
-
+        console.log("Correct: "+usrCorrectAns);
     }else{
         document.getElementById("answerResponse").innerHTML = "<span style='color: red'>Incorrect</span>";
         usrIncorrectAns+=1;
-         PlayersData.splice((PlayersData.length)-1,1,usrIncorrectAns);
-    }	
+        PlayersData.splice((PlayersData.length)-1,1,usrIncorrectAns);
+         console.log("Incorrect: " + usrIncorrectAns);
+    }
+    showall();
+}
+function findPercentageScore(){
+    document.getElementById('modalBgChart').className += ' activateModalChart';
+
+    document.getElementById("showPercentage").innerHTML = "";
+    usrQuestionCount = usrCorrectAns+usrIncorrectAns;
+    console.log(usrQuestionCount);
+    var usrPercentage=((usrCorrectAns)/usrQuestionCount)*100;
+    console.log(usrPercentage);
+    var date=new Date();
+    var showValue = `Total number of questions: ${usrQuestionCount}\nPercentage score: ${Math.floor(usrPercentage)}%\nCurrent date: ${date}`;
+    console.log(showValue);
+
+    document.getElementById("showPercentage").innerHTML = showValue;
+    resetForm();
+    disableModal();
 }
